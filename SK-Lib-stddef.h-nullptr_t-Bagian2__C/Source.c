8665617c3d8a29489fb3f9d1770529e546fbe23f
Rizky Khapidsyah <rizkykhapidsyah@gmail.com>
2022-01-08 07:26:12 +0700

    Add project files.

---
+++ SK-Lib-stddef.h-nullptr_t-Bagian2__C/Source.c
@@ -0,0 +1,17 @@
+// This program compiles (may produce warning)
+
+#include <stdio.h>
+#include <conio.h>
+
+/*
+	Source by CPlusPlus
+	Modified For Learn by RK
+	I.D.E : VS2019
+*/
+
+int main()
+{
+	int x = NULL;
+	_getch();
+	return 0;
+}
